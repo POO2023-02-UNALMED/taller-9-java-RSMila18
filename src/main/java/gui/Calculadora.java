ead60e60ac324a757f87cfacaecf2bb5a34b5b63
Camila Rios <mriosm@unal.edu.co>
2023-11-04 18:45:40 -0500

    1

--- src/main/java/gui/Calculadora.java
+++ src/main/java/gui/Calculadora.java
@@ -2,24 +2,29 @@ package gui;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Calculadora extends Application  {
 	
 	public static void main(String args[]){ 
-	      ***(args); 
+		launch(args); 
 	} 
 	
 	@Override
 	public void start(Stage window) throws Exception {
-		*** vb = new VBox();
-		window.setTitle(***);
+		VBox vb = new VBox();
+		window.setTitle("Calculadora");
 		
 		vb.getChildren().add(new Calculator());
-		Scene sc = new Scene(***, 270, 270);
-		window.setScene(***);
-		***.show();
+		Scene sc = new Scene(vb, 270, 270);
+		window.setScene(sc);
+		window.show();
 	}
 	
 }
